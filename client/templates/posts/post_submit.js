ddf0713fa5dc4ae024f1ee26279ba48f78ac72ac
Wes <wesley.ch.yu@gmail.com>
2015-08-08 22:39:17 -0700

    Post owners can see notifications in header when a comment is received

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,15 +1,15 @@
-Template.postSubmit.onCreated = function () {
+Template.postSubmit.onCreated(function() {
   Session.set('postSubmitErrors', {});
-}
+});
 
 Template.postSubmit.helpers({
   errorMessage: function(field) {
     return Session.get('postSubmitErrors')[field];
   },
-  errorClass: function(field) {
+  errorClass: function (field) {
     return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
   }
-})
+});
 
 Template.postSubmit.events({
   'submit form': function(e) {
@@ -37,4 +37,3 @@ Template.postSubmit.events({
     });
   }
 });
-
